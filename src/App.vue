0fc4491a257c072f328acd4d0acba404d1b1aece
Sandra <sandra@gmail.com>
2018-04-25 19:20:57 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div>
+   
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/zoo.vue'
 
 export default {
   name: 'app',
---
+++ src/components/zoo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <th>Species</th>
+        <th>Name</th>
+        <th>Date</th>
+      </thead>
+      <tbody>
+      <tr v-for="(animal, key) in animals" :key="key">
+        <td>{{ animal.species }}</td>
+        <td>{{ animal.name }}</td>
+        <td>{{ animal.date }}</td>
+      </tr>
+      </tbody>
+    </table>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+ data(){
+   return{
+     animals: [
+       {species:'lion', name:'Simba', date: '2016'},
+       {species:'monkey', name:'Chita', date: '2017'},
+       {species:'fish', name:'Nemo', date: ''},
+       {species:'iguana', name:'Kamy', date: '2018'},
+       {species:'snake', name:'Set', date: ''}
+     ]
+   }
+ }
+}
+</script>
+
